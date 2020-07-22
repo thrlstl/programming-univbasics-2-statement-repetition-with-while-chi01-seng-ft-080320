a5a355fa9529020a21dff47137c5256bea5ba85c
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-22 20:45:40 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,4 @@
-# Write your code here
+count = 0 # A bit of data defined outside the loop
+while count < 3 do # A Boolean expression using the bit of data
+  puts "I am the #{count}, I love to count!" # Work
+end
